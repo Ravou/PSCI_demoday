9f4f0461dd537cf48d7f904926e28f8cdfd202f8
Ravou <oliviaravou@hotmail.fr>
2025-09-30 11:23:48 +0200

    add app.js

---
+++ front_end_olivia/app.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="navbar">
+        <div className="logo">PSCI:wq
+	  </div>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+        <button className="btn-cta">Get Started</button>
+      </nav>
+      <header className="hero">
+        <h1>Cyber Defense That Evolves Daily.</h1>
+        <p>AI-driven protection that learns, adapts, and grows stronger</p>
+        <button className="btn-primary">Get Protected Today</button>
+        <button className="btn-secondary">How Oryn Works</button>
+      </header>
+      <main>
+        <section className="services">
+          <div className="card">
+            <h3>Cloud Security & Compliance</h3>
+            <button className="btn-cta">Get Started</button>
+          </div>
+          <div className="card">
+            <h3>AI-Driven Risk Intelligence</h3>
+            <button className="btn-cta">Get Started</button>
+          </div>
+          <div className="card">
+            <h3>Cloud Security & Compliance</h3>
+            <button className="btn-cta">Get Started</button>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+

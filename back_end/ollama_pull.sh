a08b2f3761569f9d7bf9ca0168d7e8f29730852a
Ravou <oliviaravou@hotmail.fr>
2025-10-20 11:07:52 +0200

    Remove ollama_pull.sh

--- back_end/ollama_pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-  echo "Tentative de téléchargement de l’image Ollama..."
-  docker pull ollama/ollama:latest && break
-  echo "Échec du téléchargement. Nouvelle tentative dans 15 secondes..."
-  sleep 15
-done
